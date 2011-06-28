a275bbbe676be97081687cb9924947e1760e5556
youngking <yanckin@gmail.com>
2011-06-28 17:01:58 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import os
+
+def read_file(*path):
+    base_dir = os.path.dirname(__file__)
+    file_path = (base_dir, ) + tuple(path)
+    return file(os.path.join(*file_path)).read()
+
+setup(
+    name = "flask-cn",
+    url = "http://flask.flyzen.com",
+    license="BSD",
+    author = "Young King",
+    author_email = "yanckin@gmail.com",
+    description = "Flask documents (chinese translation)",
+    long_description = (
+        read_file("README") + "\n\n"),
+    version = "0.6",
+    packages = [],
+    include_package_data = True, 
+    zip_safe=False,
+    install_requires=['Flask',],
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
